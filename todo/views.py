c44fcb80664f9affd9ddf76b3b2421304dee7111
quan3721 <hongquan.nguyen3007@gmail.com>
2023-11-21 19:05:59 +0700

    todo app final

--- todo/views.py
+++ todo/views.py
@@ -5,8 +5,12 @@ def home(request):
     
     tasks = Task.objects.filter(is_completed=False).order_by('-updated_at') # get objects following the condition
     # print(tasks)
+    
+    completed_tasks = Task.objects.filter(is_completed=True)
+
     context = {
-        'tasks': tasks
+        'tasks': tasks,
+        'completed_tasks': completed_tasks,
     }
     
     return render(request, 'home-todo.html', context)
--- todo_app/urls.py
+++ todo_app/urls.py
@@ -2,5 +2,18 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('addTask/', views.addTask, name='addTask')
+    # Add Task
+    path('addTask/', views.addTask, name='addTask'),
+    
+    # Mark as Done #
+    path('markasdone/<int:pk>', views.mark_as_done, name='mark_as_done'),
+    
+    # Mark as Undone
+    path('markasundone/<int:pk>', views.mark_as_undone, name='mark_as_undone'),
+    
+    # EDIT Feature #
+    path('edit_task/<int:pk>', views.edit_task, name='edit_task'),
+    
+    # Delete task #
+    path('delete_task/<int:pk>', views.delete_task, name='delete_task'),
 ]
